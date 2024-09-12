6a88e0929b3ac1ac3b78ea71578983b09a2dca21
riccardo ceccarani <riccardo.ceccarani00@gmail.com>
2024-09-12 15:25:37 +0200

    updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@
     prefix: "",
     theme: {
     	container: {
-    		center: 'true',
     		padding: '2rem',
     		screens: {
     			sm: '320px',
@@ -48,49 +47,9 @@
     			publishButton: '#0005FA',
     			backgroundButton: '#2b3440'
     		},
-    		keyframes: {
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out',
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		}
     	}
     },
-    plugins: [require("tailwindcss-animate"),require('daisyui'),],
+    plugins: [require("tailwindcss-animate")],
   } satisfies Config
 
   export default config
